e7610e537f45de55ba6d395af238d41ab05a8fea
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-22 13:29:14 +0100

    feat: add trash button add card layout

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,23 @@
-const Card = () => {
+const Card = (props) => {
+  const { title, content, image, tags } = props.post;
+
   return (
-    <div className="row">
-      <div className="card col-12 col-md-4">
-        <img src="" alt="" />
+    <div className="col-12 col-md-4">
+      <div className="card my-3">
+        <img src={image} alt={title} />
         <div className="card-body">
-          <h3 className="title-card">Titolo</h3>
-          <div className="title-content">Descrizione</div>
-          <div className="title-tags">Tags</div>
+          <h3 className="title-card mt-3 mb-3">{title}</h3>
+          <div className="title-content">
+            <strong>DESCRIZIONE: </strong>
+            {content}
+          </div>
+          <div className="title-tags">
+            <strong>TAGS: </strong>
+            {tags.join(", ")}
+          </div>
+          <div className="btn btn-danger mt-3">
+            <i className="fa-solid fa-trash-can"></i>Elimina
+          </div>
         </div>
       </div>
     </div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
-
-const baseApiUrl = "http://localhost:3000";
-
-const initialDefault = {
-  title: "",
-  content: "",
-  image: "",
-  tags: "",
-};
+import axios from "axios";
 
 const App = () => {
+  const baseApiUrl = "http://localhost:3000";
+
+  const initialDefault = {
+    title: "",
+    content: "",
+    image: "",
+    tags: "",
+  };
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    axios
+      .get(`${baseApiUrl}/posts`)
+      .then((res) => {
+        setPosts(res.data);
+      })
+
+      .catch((error) => {
+        console.error("Errore nel caricamento dei post", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <>
-      <div className="container">
+      <div className="container my-3">
+        <h1 className="text-center">Gestione Post</h1>
         <div className="row">
-          <Card />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
         </div>
       </div>
     </>
